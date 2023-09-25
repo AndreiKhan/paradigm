4ab13d83a91addc905bea537354da8493d75c4fe
«Andrei» <Spectacul9r@mail.ru>
2023-09-25 18:46:05 +0500

    HW6

--- Seminar_6/HomeWork.py
+++ Seminar_6/HomeWork.py
@@ -1,7 +1,6 @@
 def binary(arr, number):
     arr_start = 0
     arr_end = len(arr) - 1
-    i = 0
     
     while arr_start <= arr_end:
         
@@ -15,8 +14,6 @@ def binary(arr, number):
             arr_end = arr_center - 1
         else:
             arr_start = arr_center + 1
-
-        i += 1
     
     return -1
 
