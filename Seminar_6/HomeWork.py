d4d5ac6d3dfd1f2bf6baead5d5051c4572e68f8a
«Andrei» <Spectacul9r@mail.ru>
2023-09-25 20:16:00 +0500

    HW6

--- Seminar_6/HomeWork.py
+++ Seminar_6/HomeWork.py
@@ -9,34 +9,42 @@ def stopWatch():
     print('4 - остановить секундомер')
     print()
 
+    # Переменные для запоминания времени, последней команды и остановили ли секундомер
     startTime = 0
     pause = 0
     lastCommand = 0
     start = True
 
+    # бесконечный цикл пока его не остановят
     while start:
         command = input()
 
+        # Подкорректированы условия чтобы не сломать секундомер
+        # Запустить можно только один раз первой командой
         if command == '1' and lastCommand == 0:
             startTime = time.time()
             lastCommand = '1'
             print('Секундомер запущен')
 
+        # Приостановить можно только если секундомер запущен
         elif command == '2' and lastCommand != '2' and lastCommand != 0:
             pause += (round(time.time() - startTime, 2))
             lastCommand = '2'
             print(f'Секундомер приостановлен, текущее время - {round(pause, 2)}')
 
+        # Снять с паузы можно только если секундомер был приостановлен
         elif command == '3' and lastCommand == '2':
             startTime = time.time()
             lastCommand = '3'
             print(f'Секундомер снят с паузы')
 
+        # Остановить можно только если секундомер запущен
         elif command == '4' and lastCommand != 0 and lastCommand != '2':
             print(f'Времени прошло {round((time.time() - startTime) + pause, 2)}')
             print('Секундомер остановлен')
             start = False
 
+        # Если введено чтото не то, то выдаст сообщение
         elif True:
             print('Нельзя это сделать')
 
