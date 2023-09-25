132ea2ccf72d1ff8f9e7d98549390806b183f416
«Andrei» <Spectacul9r@mail.ru>
2023-09-25 19:51:16 +0500

    HW6

--- Seminar_6/HomeWork.py
+++ Seminar_6/HomeWork.py
@@ -1,23 +1,36 @@
-def binary(arr, number):
-    arr_start = 0
-    arr_end = len(arr) - 1
-    
-    while arr_start <= arr_end:
-        
-        arr_center = (arr_start + arr_end) // 2
-        search_number = arr[arr_center]
-        
-        if search_number == number:
-            return arr_center
-        
-        if search_number > number:
-            arr_end = arr_center - 1
-        else:
-            arr_start = arr_center + 1
-    
-    return -1
+import time
 
-array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-number = 7
+def stopWatch():
+    # Меню
+    print('Введите цифру:')
+    print('1 - запустить секундомер')
+    print('2 - приостановить секундомер')
+    print('3 - заново запустить секундомер')
+    print('4 - остановить секундомер')
+    print()
 
-print(f'Индекс числа {number} в массиве = {binary(array, number)}')
+    startTime = 0
+    pause = 0
+    start = True
+
+    while start:
+        command = input()
+
+        if command == '1':
+            print('Секундомер запущен')
+            startTime = time.time()
+
+        elif command == '2':
+            pause += (round(time.time() - startTime, 2))
+            print(f'Секундомер приостановлен, текущее время - {round(pause, 2)}')
+
+        elif command == '3':
+            startTime = time.time()
+            print(f'Секундомер запущен')
+
+        elif command == '4':
+            print(f'Времени прошло {round((time.time() - startTime) + pause, 2)}')
+            print('Секундомер остановлен')
+            start = False
+
+stopWatch()
