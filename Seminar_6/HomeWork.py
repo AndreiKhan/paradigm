311fad3748dae146a784392ed8d55c14300aa26f
«Andrei» <Spectacul9r@mail.ru>
2023-09-25 20:11:24 +0500

    HW6

--- Seminar_6/HomeWork.py
+++ Seminar_6/HomeWork.py
@@ -11,26 +11,33 @@ def stopWatch():
 
     startTime = 0
     pause = 0
+    lastCommand = 0
     start = True
 
     while start:
         command = input()
 
-        if command == '1':
+        if command == '1' and lastCommand == 0:
             startTime = time.time()
+            lastCommand = '1'
             print('Секундомер запущен')
 
-        elif command == '2':
+        elif command == '2' and lastCommand != '2' and lastCommand != 0:
             pause += (round(time.time() - startTime, 2))
+            lastCommand = '2'
             print(f'Секундомер приостановлен, текущее время - {round(pause, 2)}')
 
-        elif command == '3':
+        elif command == '3' and lastCommand == '2':
             startTime = time.time()
+            lastCommand = '3'
             print(f'Секундомер снят с паузы')
 
-        elif command == '4':
+        elif command == '4' and lastCommand != 0 and lastCommand != '2':
             print(f'Времени прошло {round((time.time() - startTime) + pause, 2)}')
             print('Секундомер остановлен')
             start = False
+
+        elif True:
+            print('Нельзя это сделать')
 
 stopWatch()
